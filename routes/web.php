1e1cf5587dceed91be025d899386e2e340a74595
Evander Lucena <evander.willian@gmail.com>
2020-03-10 01:13:43 -0300

    Enter

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('produtos', 'ProdutoController');
+/*Route::delete('produtos/{id}', 'ProdutoController@destroy')->name('produtos.destroy');
+Route::put('produtos/{id}', 'ProdutoController@update')->name('produtos.update');
+Route::get('produtos/create', 'ProdutoController@create')->name('produtos.create');
+Route::get('produtos/{id}/edit', 'ProdutoController@edit')->name('produtos.edit');
+Route::get('produtos/{id}', 'ProdutoController@show')->name('produtos.show');
+Route::get('produtos', 'ProdutoController@index')->name('produtos.index');
+Route::post('produtos', 'ProdutoController@save')->name('produtos.save');*/
+
 
 Route::get('/', function () {
     return view('welcome');
